9057c2778aa69dfc85bae7cc470af822c1f075d0
royalharry007 <harichandchauhan78@gmail.com>
2025-08-15 10:39:07 +0530

    backend with some ejs

--- index.js
+++ index.js
@@ -1,21 +1,35 @@
 const { log } = require('console');
+const { name } = require('ejs');
 const express = require('express')
 
 const app = express()
 const port = 3000;
 
+// we are setting the view engine to ejs to use ejs in our sytem 
 app.set("view engine", "ejs")
-// app.use((req , res)=>{
-//     console.log(" reqest received ")
-//     res.send(" this is response ")
-// })
 
-app.get("/",(req, res) =>{
-    let diceVal = Math.floor(Math.random() *6 ) + 1;
-    res.render("home.ejs",{num : diceVal});
+app.get("/ig/:username", (req, res) => {
+
+    const instaData = require("./data.json")
+    let username = req.params.username;
+    let data = instaData[username];
+
+
+    if (username) {
+     res.render("instagram.ejs", { data });
+
+    }
+    else{
+        res.rende("error.ejs")
+    }
+
+
+
+  
 
 })
 
+// making a connection to listen the port 
 app.listen(port, () => {
     console.log(" port is listening ")
 
